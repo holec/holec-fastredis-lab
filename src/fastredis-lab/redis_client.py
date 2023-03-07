cafdf61a22fcabe2aa38d0b3ddb02a58ca92e4ed
nerd pingu <nerd.pingu@gmail.com>
2023-03-07 08:20:36 +0100

    Reformating code with black

--- src/fastredis-lab/redis_client.py
+++ src/fastredis-lab/redis_client.py
@@ -5,12 +5,13 @@ import aioredis
 
 
 async def main():
-    redis = await aioredis.create_redis_pool('redis://0.0.0.0')
-    await redis.set('my-key', 'value')
-    value = await redis.get('my-key', encoding='utf-8')
+    redis = await aioredis.create_redis_pool("redis://0.0.0.0")
+    await redis.set("my-key", "value")
+    value = await redis.get("my-key", encoding="utf-8")
     print(value)
 
     redis.close()
     await redis.wait_closed()
+
 
 asyncio.run(main())
